5da6a300c24c9fb1d3c3e3be90547c16c0dd2959
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-07-19 22:01:01 -0300

    :hammer: Adjustments for build

--- src/Deshboard.js
+++ src/Deshboard.js
@@ -2,17 +2,21 @@ import './App.css';
 import React, { useState, useEffect } from 'react'
 import api from './Services/api'
 import Coin from './Components/Coin';
+import Spinner from "react-spinkit";
 
 
 function Deshboard() {
     const [coins, setCoins] = useState([]);
     const [search, setSearch] = useState('');
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true)
         api
             .get('markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
             .then(res => {
                 setCoins(res.data)
+                setLoading(false)
             })
             .catch(error => console.log(error))
     }, [])
@@ -25,30 +29,37 @@ function Deshboard() {
 
     return (
         <div className="card-coin">
-            <div className="coin-search">
-                <h1 className="coin-text">Search a currency</h1>
-                <form>
-                    <input type="text" placeholder='Search' className="coin-input" onChange={handleChange} />
-                </form>
-            </div>
-            <div className="inner">
-                <div className="row">
-                    {filteredCoins.map(({ id, name, image, symbol, current_price, market_cap, total_volume, price_change_percentage_24h }) => {
-                        return (
-                            <Coin
-                                key={id}
-                                image={image}
-                                name={name}
-                                symbol={symbol}
-                                price={current_price}
-                                marketcap={market_cap}
-                                priceChange={price_change_percentage_24h}
-                                volume={total_volume}
-                                id={id} />
-                        )
-                    })}
-                </div>
-            </div>
+            {
+                loading ? <div className="lodingScrean"> <Spinner name="pacman" fadeIn="none" color="#EF9444" /></div>
+                    :
+                    <>
+                        <div className="coin-search">
+                            <h1 className="coin-text">Search a currency</h1>
+                            <form>
+                                <input type="text" placeholder='Search' className="coin-input" onChange={handleChange} />
+                            </form>
+                        </div>
+                        <div className="inner">
+                            <div className="row">
+                                {filteredCoins.map(({ id, name, image, symbol, current_price, market_cap, total_volume, price_change_percentage_24h }) => {
+                                    return (
+                                        <Coin
+                                            key={id}
+                                            image={image}
+                                            name={name}
+                                            symbol={symbol}
+                                            price={current_price}
+                                            marketcap={market_cap}
+                                            priceChange={price_change_percentage_24h}
+                                            volume={total_volume}
+                                            id={id}
+                                        />
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </>
+            }
         </div>
     );
 }
